8d005d4d8352be3d250201d8f591b802ae91bb81
Sean Gillespie <sean@swgillespie.me>
2021-09-29 23:55:48 -0700

    Move test harness to a4 directory

---
+++ a4/tests.py
@@ -0,0 +1,97 @@
+# Copyright 2021 Sean Gillespie.
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+"""
+a4's test harness, for external testing.
+"""
+
+import json
+import os
+import subprocess
+import sys
+
+
+def collect_tests(dir):
+    tests = []
+    for (path, _, files) in os.walk(dir):
+        for file in files:
+            _, ext = os.path.splitext(file)
+            if ext != ".json":
+                continue
+            with open(os.path.join(path, file)) as f:
+                test = json.load(f)
+                if test["kind"] == "perft":
+                    for key, value in test["counts"].items():
+                        tests.append(
+                            {
+                                "kind": "perft",
+                                "fen": test["fen"],
+                                "depth": int(key),
+                                "count": value,
+                            }
+                        )
+    return tests
+
+
+def run_perft_test(test):
+    assert test["kind"] == "perft"
+    count = int(
+        subprocess.check_output(
+            [
+                "./target/release/a4-perft",
+                test["fen"],
+                "--depth",
+                str(test["depth"]),
+            ]
+        )
+        .decode("utf-8")
+        .strip()
+    )
+    return {
+        "test": test,
+        "pass": test["count"] == count,
+        "expected": test["count"],
+        "actual": count,
+    }
+
+
+def main():
+    tests = collect_tests(os.path.join(os.getcwd(), "tests"))
+    passes = []
+    fails = []
+    for test in tests:
+        result = run_perft_test(test)
+        if result["pass"]:
+            print(".", end="")
+            passes.append(result)
+        else:
+            print("!", end="")
+            fails.append(result)
+        sys.stdout.flush()
+    print("\n")
+    if fails:
+        print("=====================================")
+        print("Failed Tests:")
+    for fail in fails:
+        assert fail["test"]["kind"] == "perft"
+        print(
+            "  perft: {} (depth {}) => {} (expected {})".format(
+                fail["test"]["fen"],
+                fail["test"]["depth"],
+                fail["actual"],
+                fail["expected"],
+            )
+        )
+    if fails:
+        print("=====================================")
+    print(f"{len(passes)}/{len(tests)} passed")
+    sys.exit(0 if len(passes) == len(tests) else 1)
+
+
+if __name__ == "__main__":
+    main()
