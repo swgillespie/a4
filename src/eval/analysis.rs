432bf3303188a51d93af3c3687fcc0b61b60cf7f
Sean Gillespie <sean@swgillespie.me>
2022-04-30 13:31:12 -0700

    [eval] Re-write evaluation
    In anticipation of making our evaluation function significantly more
    complex, this commit introduces some skeleton code for how we might
    write evaluation that considers many different features.

--- src/eval/analysis.rs
+++ src/eval/analysis.rs
@@ -5,6 +5,9 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
+
+use std::lazy::OnceCell;
+
 use crate::{
     core::{
         SquareSet, SS_FILES, SS_FILE_A, SS_FILE_B, SS_FILE_C, SS_FILE_D, SS_FILE_E, SS_FILE_F,
@@ -14,132 +17,188 @@ use crate::{
     position::Position,
 };
 
+struct OnceAnalysis<T> {
+    white: OnceCell<T>,
+    black: OnceCell<T>,
+}
+
+impl<T> OnceAnalysis<T> {
+    pub fn new() -> OnceAnalysis<T> {
+        OnceAnalysis {
+            white: OnceCell::new(),
+            black: OnceCell::new(),
+        }
+    }
+
+    pub fn get_or_init<F>(&self, color: Color, f: F) -> &T
+    where
+        F: FnOnce() -> T,
+    {
+        let cell = match color {
+            Color::White => &self.white,
+            Color::Black => &self.black,
+        };
+
+        cell.get_or_init(f)
+    }
+}
+
 /// Provider of common board analyses upon a static position. It is suitable for use in board
 /// evaluators, where analysis queries can be aggressively cached when evaluating a single,
 /// immutable board position.
 pub struct Analysis<'a> {
     pos: &'a Position,
+    doubled_pawns: OnceAnalysis<SquareSet>,
+    isolated_pawns: OnceAnalysis<SquareSet>,
+    backward_pawns: OnceAnalysis<SquareSet>,
+    moves: OnceAnalysis<Vec<Move>>,
 }
 
 impl<'a> Analysis<'a> {
     pub fn new(pos: &'a Position) -> Analysis<'a> {
-        Analysis { pos }
+        Analysis {
+            pos,
+            doubled_pawns: OnceAnalysis::new(),
+            isolated_pawns: OnceAnalysis::new(),
+            backward_pawns: OnceAnalysis::new(),
+            moves: OnceAnalysis::new(),
+        }
     }
 
-    /// Returns the set of doubled pawns left by the given color.
     pub fn doubled_pawns(&self, color: Color) -> SquareSet {
-        let pawns = self.pos.pawns(color);
-        let mut answer = SquareSet::empty();
-        for &file in &SS_FILES {
-            let pawns_on_file = pawns.and(file);
-            if pawns_on_file.len() > 1 {
-                answer = answer.or(pawns_on_file);
-            }
-        }
-
-        answer
-    }
-
-    /// Returns the set of backward pawns left by the given color.
-    pub fn backward_pawns(&self, color: Color) -> SquareSet {
-        fn walk_rank<I>(
-            iter: I,
-            current_file_pawns: SquareSet,
-            adjacent_file_pawns: SquareSet,
-        ) -> SquareSet
-        where
-            I: Iterator<Item = SquareSet>,
-        {
-            let mut answer = SquareSet::empty();
-            for rank in iter {
-                let current_file_rank = rank.and(current_file_pawns);
-                let adjacent_file_rank = rank.and(adjacent_file_pawns);
-                if !current_file_rank.is_empty() && adjacent_file_rank.is_empty() {
-                    answer = answer.or(current_file_rank);
-                    break;
-                }
-
-                if !adjacent_file_rank.is_empty() && current_file_rank.is_empty() {
-                    break;
-                }
-            }
-
-            answer
-        }
-
-        let pawns = self.pos.pawns(color);
-        let mut answer = SquareSet::empty();
-        for file in files() {
-            let adj_files = adjacent_files(file);
-            let current_file = SquareSet::all().file(file);
-            let pawns_on_current_file = pawns.and(current_file);
-            let pawns_on_adjacent_files = pawns.and(adj_files);
-            if pawns_on_current_file.is_empty() {
-                continue;
-            }
-
-            let file_answer = match color {
-                Color::White => walk_rank(
-                    SS_RANKS.iter().cloned(),
-                    pawns_on_current_file,
-                    pawns_on_adjacent_files,
-                ),
-                Color::Black => walk_rank(
-                    SS_RANKS.iter().cloned().rev(),
-                    pawns_on_current_file,
-                    pawns_on_adjacent_files,
-                ),
-            };
-
-            answer = answer.or(file_answer);
-        }
-
-        answer
+        self.doubled_pawns
+            .get_or_init(color, || doubled_pawns(self.pos, color))
+            .clone()
     }
 
     pub fn isolated_pawns(&self, color: Color) -> SquareSet {
-        let pawns = self.pos.pawns(color);
+        self.isolated_pawns
+            .get_or_init(color, || isolated_pawns(self.pos, color))
+            .clone()
+    }
+
+    pub fn backward_pawns(&self, color: Color) -> SquareSet {
+        self.backward_pawns
+            .get_or_init(color, || backward_pawns(self.pos, color))
+            .clone()
+    }
+
+    pub fn moves(&self, color: Color) -> &[Move] {
+        self.moves.get_or_init(color, || {
+            // Our move generator only operates on the current side to move. If we need to analyze the
+            // other side, make a null move and analyze that instead.
+            let pos = if self.pos.side_to_move() != color {
+                self.pos.clone_and_make_move(Move::null())
+            } else {
+                self.pos.clone()
+            };
+
+            assert!(pos.side_to_move() == color);
+            let mut moves = Vec::new();
+            movegen::generate_moves(pos.side_to_move(), &pos, &mut moves);
+            moves.retain(|mov| pos.is_legal_given_pseudolegal(*mov));
+            moves
+        })
+    }
+
+    pub fn mobility(&self, color: Color) -> usize {
+        self.moves(color).len()
+    }
+
+    pub fn position(&self) -> &Position {
+        self.pos
+    }
+}
+
+/// Returns the set of doubled pawns left by the given color.
+fn doubled_pawns(pos: &Position, color: Color) -> SquareSet {
+    let pawns = pos.pawns(color);
+    let mut answer = SquareSet::empty();
+    for &file in &SS_FILES {
+        let pawns_on_file = pawns.and(file);
+        if pawns_on_file.len() > 1 {
+            answer = answer.or(pawns_on_file);
+        }
+    }
+
+    answer
+}
+
+/// Returns the set of backward pawns left by the given color.
+fn backward_pawns(pos: &Position, color: Color) -> SquareSet {
+    fn walk_rank<I>(
+        iter: I,
+        current_file_pawns: SquareSet,
+        adjacent_file_pawns: SquareSet,
+    ) -> SquareSet
+    where
+        I: Iterator<Item = SquareSet>,
+    {
         let mut answer = SquareSet::empty();
-        for file in files() {
-            let adj_files = adjacent_files(file);
-            let current_file = SquareSet::all().file(file);
-            let pawns_on_current_file = pawns.and(current_file);
-            let pawns_on_adjacent_file = pawns.and(adj_files);
-            if pawns_on_current_file.is_empty() {
-                continue;
+        for rank in iter {
+            let current_file_rank = rank.and(current_file_pawns);
+            let adjacent_file_rank = rank.and(adjacent_file_pawns);
+            if !current_file_rank.is_empty() && adjacent_file_rank.is_empty() {
+                answer = answer.or(current_file_rank);
+                break;
             }
 
-            if pawns_on_adjacent_file.is_empty() {
-                answer = answer.or(pawns_on_current_file);
+            if !adjacent_file_rank.is_empty() && current_file_rank.is_empty() {
+                break;
             }
         }
 
         answer
     }
 
-    pub fn mobility(&self, color: Color) -> u32 {
-        // Our move generator only operates on the current side to move. If we need to analyze the
-        // other side, make a null move and analyze that instead.
-        let pos = if self.pos.side_to_move() != color {
-            let mut copied_pos = self.pos.clone();
-            copied_pos.make_move(Move::null());
-            copied_pos
-        } else {
-            self.pos.clone()
-        };
-
-        assert!(pos.side_to_move() == color);
-        let mut moves = Vec::new();
-        movegen::generate_moves(pos.side_to_move(), &pos, &mut moves);
-        let mut count = 0;
-        for &mov in &moves {
-            if pos.is_legal_given_pseudolegal(mov) {
-                count += 1;
-            }
+    let pawns = pos.pawns(color);
+    let mut answer = SquareSet::empty();
+    for file in files() {
+        let adj_files = adjacent_files(file);
+        let current_file = SquareSet::all().file(file);
+        let pawns_on_current_file = pawns.and(current_file);
+        let pawns_on_adjacent_files = pawns.and(adj_files);
+        if pawns_on_current_file.is_empty() {
+            continue;
         }
 
-        count
+        let file_answer = match color {
+            Color::White => walk_rank(
+                SS_RANKS.iter().cloned(),
+                pawns_on_current_file,
+                pawns_on_adjacent_files,
+            ),
+            Color::Black => walk_rank(
+                SS_RANKS.iter().cloned().rev(),
+                pawns_on_current_file,
+                pawns_on_adjacent_files,
+            ),
+        };
+
+        answer = answer.or(file_answer);
     }
+
+    answer
+}
+
+fn isolated_pawns(pos: &Position, color: Color) -> SquareSet {
+    let pawns = pos.pawns(color);
+    let mut answer = SquareSet::empty();
+    for file in files() {
+        let adj_files = adjacent_files(file);
+        let current_file = SquareSet::all().file(file);
+        let pawns_on_current_file = pawns.and(current_file);
+        let pawns_on_adjacent_file = pawns.and(adj_files);
+        if pawns_on_current_file.is_empty() {
+            continue;
+        }
+
+        if pawns_on_adjacent_file.is_empty() {
+            answer = answer.or(pawns_on_current_file);
+        }
+    }
+
+    answer
 }
 
 fn adjacent_files(file: File) -> SquareSet {
--- src/eval/eval.rs
+++ src/eval/eval.rs
@@ -11,148 +11,167 @@ use crate::{
     position::Position,
 };
 
-const KING_WEIGHT: i16 = 10000;
 const QUEEN_WEIGHT: i16 = 900;
 const ROOK_WEIGHT: i16 = 500;
 const BISHOP_WEIGHT: i16 = 300;
 const KNIGHT_WEIGHT: i16 = 300;
 const PAWN_WEIGHT: i16 = 100;
-const PAWN_FORMATION_WEIGHT: i16 = 50;
 const MOBILITY_WEIGHT: i16 = 10;
 
-pub fn evaluate(pos: &Position) -> Value {
-    let analysis = Analysis::new(pos);
+// Pawn piece modifiers
+const ISOLATED_PAWN_MODIFIER: i16 = 17;
+const BACKWARD_PAWN_MODIFIER: i16 = 10;
+const DOUBLED_PAWN_MODIFIER: i16 = 10;
 
-    // Check out mobility first - it's possible that a side has been checkmated.
-    let white_mobility = analysis.mobility(Color::White);
-    if white_mobility == 0 {
-        if pos.is_check(Color::White) {
-            return Value::mated_in(0);
-        } else {
-            return Value::new(0);
+pub struct Evaluator<'a> {
+    analysis: Analysis<'a>,
+    mobility: [i16; 2],
+    material: [i16; 2],
+    pawn_modifiers: [i16; 2],
+    #[cfg(feature = "trace-eval")]
+    remarks: Vec<(Square, &'static str)>,
+}
+
+impl<'a> Evaluator<'a> {
+    fn new(pos: &'a Position) -> Evaluator<'a> {
+        Evaluator {
+            analysis: Analysis::new(pos),
+            mobility: [0; 2],
+            material: [0; 2],
+            pawn_modifiers: [0; 2],
+            #[cfg(feature = "trace-eval")]
+            remarks: vec![],
         }
     }
-    let black_mobility = analysis.mobility(Color::Black);
-    if black_mobility == 0 {
-        if pos.is_check(Color::Black) {
-            return Value::mate_in(0);
-        } else {
-            return Value::new(0);
+
+    fn evaluate(&mut self) -> Value {
+        // Check out mobility first - it's possible that a side has been checkmated.
+        let white_mobility = self.analysis.mobility(Color::White);
+        if white_mobility == 0 {
+            if self.analysis.position().is_check(Color::White) {
+                return Value::mated_in(0);
+            } else {
+                return Value::new(0);
+            }
         }
+        let black_mobility = self.analysis.mobility(Color::Black);
+        if black_mobility == 0 {
+            if self.analysis.position().is_check(Color::Black) {
+                return Value::mate_in(0);
+            } else {
+                return Value::new(0);
+            }
+        }
+
+        self.mobility[Color::White as usize] = white_mobility as i16 * MOBILITY_WEIGHT;
+        self.mobility[Color::Black as usize] = black_mobility as i16 * MOBILITY_WEIGHT;
+
+        for side in colors() {
+            for kind in piece_kinds() {
+                for square in self.analysis.position().pieces_of_kind(side, kind) {
+                    match kind {
+                        PieceKind::Pawn => self.evaluate_pawn(side, square),
+                        PieceKind::Knight => self.evaluate_knight(side, square),
+                        PieceKind::Bishop => self.evaluate_bishop(side, square),
+                        PieceKind::Rook => self.evaluate_rook(side, square),
+                        PieceKind::Queen => self.evaluate_queen(side, square),
+                        PieceKind::King => {}
+                    }
+                }
+            }
+        }
+
+        self.dump_evaluation();
+        let centipawns =
+            sum_terms(self.material) + sum_terms(self.mobility) + sum_terms(self.pawn_modifiers);
+        Value::new(centipawns)
     }
 
-    let (kings_w, kings_b) = evaluate_metric(
-        KING_WEIGHT,
-        |c| if pos.king(c).is_some() { 1 } else { 0 } as i16,
-    );
+    fn evaluate_knight(&mut self, side: Color, _square: Square) {
+        self.material[side as usize] += KNIGHT_WEIGHT;
+    }
 
-    let (queens_w, queens_b) = evaluate_metric(QUEEN_WEIGHT, |c| pos.queens(c).len() as i16);
-    let (rooks_w, rooks_b) = evaluate_metric(ROOK_WEIGHT, |c| pos.rooks(c).len() as i16);
-    let (bishops_w, bishops_b) = evaluate_metric(BISHOP_WEIGHT, |c| pos.bishops(c).len() as i16);
-    let (knights_w, knights_b) = evaluate_metric(KNIGHT_WEIGHT, |c| pos.knights(c).len() as i16);
-    let (pawns_w, pawns_b) = evaluate_metric(PAWN_WEIGHT, |c| pos.pawns(c).len() as i16);
-    let mobility = MOBILITY_WEIGHT * (white_mobility as i16 - black_mobility as i16);
-    let (isolated_pawns_w, isolated_pawns_b) = evaluate_metric(PAWN_FORMATION_WEIGHT, |c| {
-        -(analysis.isolated_pawns(c).len() as i16)
-    });
-    let (backward_pawns_w, backward_pawns_b) = evaluate_metric(PAWN_FORMATION_WEIGHT, |c| {
-        -(analysis.backward_pawns(c).len() as i16)
-    });
-    let (doubled_pawns_w, doubled_pawns_b) = evaluate_metric(PAWN_FORMATION_WEIGHT, |c| {
-        -(analysis.doubled_pawns(c).len() as i16)
-    });
+    fn evaluate_bishop(&mut self, side: Color, _square: Square) {
+        self.material[side as usize] += BISHOP_WEIGHT;
+    }
 
-    let value = (kings_w - kings_b)
-        + (queens_w - queens_b)
-        + (rooks_w - rooks_b)
-        + (bishops_w - bishops_b)
-        + (knights_w - knights_b)
-        + (pawns_w - pawns_b)
-        + (isolated_pawns_w - isolated_pawns_b)
-        + (backward_pawns_w - backward_pawns_b)
-        + (doubled_pawns_w - doubled_pawns_b)
-        + mobility;
+    fn evaluate_rook(&mut self, side: Color, _square: Square) {
+        self.material[side as usize] += ROOK_WEIGHT;
+    }
+
+    fn evaluate_queen(&mut self, side: Color, _square: Square) {
+        self.material[side as usize] += QUEEN_WEIGHT;
+    }
+
+    fn evaluate_pawn(&mut self, side: Color, square: Square) {
+        self.material[side as usize] += PAWN_WEIGHT;
+        if self.analysis.isolated_pawns(side).contains(square) {
+            self.pawn_modifiers[side as usize] -= ISOLATED_PAWN_MODIFIER;
+            self.remark(square, "pawn is isolated");
+        }
+
+        if self.analysis.doubled_pawns(side).contains(square) {
+            self.pawn_modifiers[side as usize] -= DOUBLED_PAWN_MODIFIER;
+            self.remark(square, "pawn is doubled");
+        }
+
+        if self.analysis.backward_pawns(side).contains(square) {
+            self.pawn_modifiers[side as usize] -= BACKWARD_PAWN_MODIFIER;
+            self.remark(square, "pawn is backward");
+        }
+    }
 
     #[cfg(feature = "trace-eval")]
-    {
+    fn remark(&mut self, square: Square, remark: &'static str) {
+        self.remarks.push((square, remark));
+    }
+
+    #[cfg(not(feature = "trace-eval"))]
+    fn remark(&mut self, _: Square, _: &'static str) {}
+
+    #[cfg(feature = "trace-eval")]
+    fn dump_evaluation(&self) {
         println!("========================================");
-        println!("FEN: {}", pos.as_fen());
+        println!("FEN: {}", self.analysis.position().as_fen());
         println!("========================================");
         println!("Term           | White | Black | Total |");
         println!("----------------------------------------");
         println!(
+            "Material       | {:^5} | {:^5} | {:^5} |",
+            self.material[Color::White as usize],
+            self.material[Color::Black as usize],
+            sum_terms(self.material)
+        );
+        println!(
             "Mobility       | {:^5} | {:^5} | {:^5} |",
-            MOBILITY_WEIGHT * white_mobility as i16,
-            MOBILITY_WEIGHT * black_mobility as i16,
-            mobility
+            self.mobility[Color::White as usize],
+            self.mobility[Color::Black as usize],
+            sum_terms(self.mobility)
         );
         println!(
-            "Kings          | {:^5} | {:^5} | {:^5} |",
-            kings_w,
-            kings_b,
-            (kings_w - kings_b)
+            "Pawn Modifiers | {:^5} | {:^5} | {:^5} |",
+            self.pawn_modifiers[Color::White as usize],
+            self.pawn_modifiers[Color::Black as usize],
+            sum_terms(self.pawn_modifiers)
         );
-        println!(
-            "Queens         | {:^5} | {:^5} | {:^5} |",
-            queens_w,
-            queens_b,
-            (queens_w - queens_b)
-        );
-        println!(
-            "Rooks          | {:^5} | {:^5} | {:^5} |",
-            rooks_w,
-            rooks_b,
-            (rooks_w - rooks_b)
-        );
-        println!(
-            "Bishops        | {:^5} | {:^5} | {:^5} |",
-            bishops_w,
-            bishops_b,
-            (bishops_w - bishops_b)
-        );
-        println!(
-            "Knights        | {:^5} | {:^5} | {:^5} |",
-            knights_w,
-            knights_b,
-            (knights_w - knights_b)
-        );
-        println!(
-            "Pawns          | {:^5} | {:^5} | {:^5} |",
-            pawns_w,
-            pawns_b,
-            (pawns_w - pawns_b)
-        );
-        println!(
-            "Isolated Pawns | {:^5} | {:^5} | {:^5} |",
-            isolated_pawns_w,
-            isolated_pawns_b,
-            (isolated_pawns_w - isolated_pawns_b)
-        );
-        println!(
-            "Backward Pawns | {:^5} | {:^5} | {:^5} |",
-            backward_pawns_w,
-            backward_pawns_b,
-            (backward_pawns_w - backward_pawns_b)
-        );
-        println!(
-            "Doubled Pawns  | {:^5} | {:^5} | {:^5} |",
-            doubled_pawns_w,
-            doubled_pawns_b,
-            (doubled_pawns_w - doubled_pawns_b)
-        );
-
-        println!("========================================");
-        println!("Total: {}", value)
+        println!("----------------------------------------");
+        println!("Remarks");
+        println!("----------------------------------------");
+        for (square, remark) in &self.remarks {
+            println!("Square {}: {}", square, remark);
+        }
     }
 
-    Value::new(value)
+    #[cfg(not(feature = "trace-eval"))]
+    fn dump_evaluation(&self) {}
 }
 
-fn evaluate_metric<F>(weight: i16, func: F) -> (i16, i16)
-where
-    F: Fn(Color) -> i16,
-{
-    (weight * func(Color::White), weight * func(Color::Black))
+fn sum_terms(terms: [i16; 2]) -> i16 {
+    terms[Color::White as usize] - terms[Color::Black as usize]
+}
+
+pub fn evaluate(pos: &Position) -> Value {
+    Evaluator::new(pos).evaluate()
 }
 
 #[cfg(test)]
